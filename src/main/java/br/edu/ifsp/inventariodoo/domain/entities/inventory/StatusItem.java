ea194f723a3f81d962352710900de4efd321e6a1
Giovana Garcia <gicagarcia58@gmail.com>
2023-11-12 18:35:38 -0300

    classe Register

---
+++ src/main/java/br/edu/ifsp/inventariodoo/domain/entities/inventory/StatusItem.java
@@ -0,0 +1,9 @@
+package br.edu.ifsp.inventariodoo.domain.entities.inventory;
+
+public enum StatusItem {
+    NEW,
+    USED,
+    DAMAGED,
+    ABSENT,
+    UNUSABLE
+}
---
+++ src/main/java/br/edu/ifsp/inventariodoo/domain/usecases/inventory/RegisterDAO.java
@@ -0,0 +1,12 @@
+package br.edu.ifsp.inventariodoo.domain.usecases.inventory;
+
+import br.edu.ifsp.inventariodoo.domain.entities.inventory.Register;
+import br.edu.ifsp.inventariodoo.domain.entities.item.Item;
+import br.edu.ifsp.inventariodoo.domain.usecases.utils.DAO;
+
+import java.util.Optional;
+
+public interface RegisterDAO extends DAO<Register, Integer> {
+    Optional<Register> findByItem(Item item);
+
+}
