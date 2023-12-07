4e156cb0455e958b03bba2720f27b76d4a0418d4
Giovana Garcia <gicagarcia58@gmail.com>
2023-12-07 15:06:10 -0300

    Telas category

---
+++ src/main/java/br/edu/ifsp/inventariodoo/application/controller/CategoryDetailUIController.java
@@ -0,0 +1,8 @@
+package br.edu.ifsp.inventariodoo.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class CategoryDetailUIController {
+    public void goToBackScene(ActionEvent actionEvent) {
+    }
+}
