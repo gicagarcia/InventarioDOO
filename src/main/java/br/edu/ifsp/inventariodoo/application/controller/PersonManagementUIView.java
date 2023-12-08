a26419810d3a6c44111068d7cc02c670855665e6
User <gicagarcia58@gmail.com>
2023-12-08 17:06:17 -0300

    Item

---
+++ src/main/java/br/edu/ifsp/inventariodoo/application/controller/PersonManagementUIView.java
@@ -0,0 +1,19 @@
+package br.edu.ifsp.inventariodoo.application.controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class PersonManagementUIView extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        final Pane graph = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("application/view/PersonManagementUI.fxml")));
+        final Scene scene = new Scene(graph, 300, 250);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
