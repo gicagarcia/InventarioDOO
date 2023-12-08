7b0bc70094adf13e82624f1d3b2ee91325781116
guizinsx <guizin51@gmail.com>
2023-12-08 17:09:02 -0300

    sqlite de inventory

--- src/main/java/br/edu/ifsp/inventariodoo/application/view/App.java
+++ src/main/java/br/edu/ifsp/inventariodoo/application/view/App.java
@@ -27,5 +27,7 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args){ launch(); }
+    public static void main(String[] args){
+        launch(args);
+    }
 }
